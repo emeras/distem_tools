bd3a28bb811d5cac3a794918949ce24bf9bbd76d
Joseph Emeras <joseph.emeras@gmail.com>
2013-10-29 11:06:50 +0100

    update setup expe

--- setup_test.rb
+++ setup_test.rb
@@ -1,6 +1,77 @@
 #!/usr/bin/ruby
 # Import the Distem module
 require 'distem'
+require 'thread'
+require 'socket'
+require 'pp'
+####
+
+class SlidingWindow
+  def initialize(size)
+    @queue = []
+    @lock = Mutex.new
+    @finished = false
+    @size = size
+  end
+
+  def add(t)
+    @queue << t
+  end
+
+  def run
+    @queue = @queue.reverse
+    tids = []
+    (1..@size).each {
+      tids << Thread.new {
+        while !@finished do
+          task = nil
+          @lock.synchronize {
+            if @queue.size > 0
+              task = @queue.pop
+            else
+              @finished = true
+            end
+          }
+          if task
+            if task.is_a?(Proc)
+              task.call
+            else
+              system(task)
+            end
+          end
+        end
+      }
+    }
+    tids.each { |tid| tid.join }
+  end
+end
+
+def port_open?(ip, port)
+  begin
+    s = TCPSocket.new(ip, port)
+    s.close
+    return true
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ETIMEDOUT
+    return false
+  end
+end
+
+def wait_ssh(host, timeout = 120)
+    def now()
+      return Time.now.to_f
+    end
+    bound = now() + timeout
+    while now() < bound do
+        t = now()
+        return true if port_open?(host, 22)
+        dt = now() - t
+        sleep(0.5 - dt) if dt < 0.5
+    end
+    return false
+end
+
+
+####
 # The path to the compressed filesystem image
 # We can point to local file since our homedir is available from NFS
 FSIMG="file:///home/ejeanvoine/public/distem/distem-fs-wheezy.tar.gz"
@@ -23,7 +94,12 @@ sshkeys = {
 }
 
 
-folding_factor = 1
+folding_factor = ARGV[0].to_i
+if (!defined? folding_factor) || folding_factor<1
+  folding_factor = 1
+end
+ipfile = "/tmp/distem_nodes_ip_#{`date +%s`}"
+iplist = []
 
 # Connect to the Distem server (on http://localhost:4567 by default)
 Distem.client do |cl|
@@ -53,9 +129,10 @@ Distem.client do |cl|
       node = "#{pnode}_#{i}"
       vnodelist << node
       cl.vnode_create(node, { 'host' => pnode }, sshkeys)
-      # ,'shared' => false, 'cow' => true
       cl.vfilesystem_create(node, { 'image' => FSIMG, 'shared' => true, 'cow' => true})
-      cl.viface_create(node, 'if0', { 'vnetwork' => vnet['name'] })
+      iface = cl.viface_create(node, 'if0', { 'vnetwork' => vnet['name'] })
+      iplist << iface['address'].split('/')[0]
+
       cl.vcpu_create(node, corenb = ncores, frequency = 1.0) if folding_factor ==1
       #cl.vmem_create(memory, swap) if folding_factor == 1
     end
@@ -63,27 +140,35 @@ Distem.client do |cl|
   end
 
   puts 'Starting VNodes'
+  puts "IPs: #{iplist}"
   # Start nodes
-  cl.vnodes_start(vnodelist, async = false)
+  cl.vnodes_start(vnodelist, async=true)
+  puts "Waiting for vnodes to be here..."
+  lock = Mutex.new
+  nb_reachable = 0
+  win = SlidingWindow.new(100)
+  iplist.each { |ip|
+    p = Proc.new {
+      exit! 1 if not wait_ssh(ip, 1200)
+      #puts "#{ip} is here"
+      lock.synchronize {
+        nb_reachable += 1
+        puts "#{nb_reachable}/#{nb_vnodes}" if ((nb_reachable % 100) == 0)
+      }
+    }
+    win.add(p)
+  }
+  win.run
+  puts "#{nb_reachable} nodes are here"
 
-  puts 'Waiting for VNodes to boot...'
-  # Now wait each virtual node to be started -- i.e. its status
-  # is set to 'RUNNING'
-  vnodelist.each do |nodename|
-    begin
-      # Getting informations about the specified virtual node
-      ret = cl.vnode_info(nodename)
-      sleep(0.2)
-    # Loops until its status is set to RUNNING
-    end until ret['status'] == Distem::Resource::Status::RUNNING
-    puts "\t#{nodename} OK"
-  end
-  # Wait one more minute, in some cases running doesn't mean ssh is available
-  sleep 60
-  puts 'Setting VNodes Network info'
-  # Fill arp table and etc hosts info
-  cl.set_global_etchosts 
+  puts "Setting global /etc/hosts"
+  cl.set_global_etchosts
+  puts "Setting global ARP tables"
   cl.set_global_arptable
+
+  File.open(ipfile,'w') do |f|
+    iplist.each{ |ip| f.puts(ip) }
+  end
 
   puts 'Finalizing VNodes Config'
   vnodelist.each do |node|
