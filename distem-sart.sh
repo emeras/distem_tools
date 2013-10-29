8e305ab7e8f354180dc7cb51d21bcd7c152ebe03
Joseph Emeras <joseph.emeras@gmail.com>
2013-10-29 11:59:01 +0100

    clean

--- distem-sart.sh
+++ distem-sart.sh
@@ -39,8 +39,11 @@ cp -r ~jemeras/public/distem/distem_experiments/charm-6.5.1 .
 
 cd charm-6.5.1/ ; ./build charm++ net-linux-x86_64 -O3
 
-cd net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; make clean ; make ; make projections'
+cd net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; make clean ; make ; make projections
 
 
+# create nodelist
 
+for i in `cat /tmp/distem_nodes_ip_*` ; do scp -rp /root/charm-6.5.1/ root@$i:; done
+./charmrun ++p 32 ++nodelist ./vnodeslist ./stencil3d 100 1
 
