9bd88cb38bf5312067c0b5bc9695125ef7a7ee37
emeras <joseph.emeras@imag.fr>
2013-11-06 09:27:06 +0100

    3 different LB in one shot -- temp feature, needed for expes

--- stencil_run.sh
+++ stencil_run.sh
@@ -10,19 +10,55 @@ TOTAL_CORES=$(($NBNODES * $NBCORES))
 #STENCIL_PARAMS='1024 32'
 STENCIL_PARAMS='8192 8192 1 256 256 1'
 
-STENCIL_OPTS=$STENCIL_PARAMS' +balancer RefineLB'
+STENCIL_OPTS=$STENCIL_PARAMS #' +balancer RefineLB'
 #STENCIL_OPTS=$STENCIL_PARAMS' +LBOff'
 
 # Erase potential older results
 for i in `cat /tmp/distem_vnodes_ip` ; do ssh root@$i "rm -f $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.*"; done
 
 # DO THIS ON FIRST VNODE
-ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/vnodeslist ./stencil3d.prj $STENCIL_OPTS"
+ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/vnodeslist ./stencil3d.prj $STENCIL_OPTS +LBOff"
 
 # grab results
-DIR=expe_`date +%s`
+DIR=expe_`date +%s`_LBOff
 mkdir $DIR
 for i in `cat /tmp/distem_vnodes_ip` ; do scp -rp root@$i:$CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.* ./$DIR; done
+
+
+
+
+
+for i in `cat /tmp/distem_vnodes_ip` ; do ssh root@$i "rm -f $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.*"; done
+
+# DO THIS ON FIRST VNODE
+ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/vnodeslist ./stencil3d.prj $STENCIL_OPTS +balancer GreedyLB"
+
+# grab results
+DIR=expe_`date +%s`_GreedyLB
+mkdir $DIR
+for i in `cat /tmp/distem_vnodes_ip` ; do scp -rp root@$i:$CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.* ./$DIR; done
+
+
+
+
+
+for i in `cat /tmp/distem_vnodes_ip` ; do ssh root@$i "rm -f $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.*"; done
+
+# DO THIS ON FIRST VNODE
+ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/ ; ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/vnodeslist ./stencil3d.prj $STENCIL_OPTS +balancer RefineLB"
+
+# grab results
+DIR=expe_`date +%s`_RefineLB
+mkdir $DIR
+for i in `cat /tmp/distem_vnodes_ip` ; do scp -rp root@$i:$CHARM_HOME/net-linux-x86_64/examples/charm++/load_balancing/stencil3d/stencil3d.prj.* ./$DIR; done
+
+
+
+
+
+
+
+
 
 
 exit 0 # continue manually
