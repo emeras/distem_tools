70af37f59f6609307fee8b89f1abaa19cf95a4d9
emeras <joseph.emeras@imag.fr>
2013-11-06 11:45:49 +0100

    use events by distrib for expe

--- distem-events.rb
+++ distem-events.rb
@@ -22,8 +22,10 @@ Distem.client do |cl|
   generator_desc['date']['max'] = 1
   generator_desc['value'] = {}
   generator_desc['value']['distribution'] = 'uniform'
-  generator_desc['value']['min'] = 1
-  generator_desc['value']['max'] = 2500
+#   generator_desc['value']['min'] = 1
+#   generator_desc['value']['max'] = 2500
+  generator_desc['value']['min'] = 1500
+  generator_desc['value']['max'] = 2000
 #  generator_desc['date'] = { 'distribution' => 'weibull', 'scale'=>10, 'shape'=>3}
   cl.event_random_add({'type' => 'vcpu', 'vnodename' => node_name}, 'power', generator_desc)
   cl.event_manager_start
@@ -32,13 +34,13 @@ Distem.client do |cl|
 end
 
 
-# Now with a trace
-Distem.client do |cl|
-  trace = {10 => 1000, 15 => 2500, 20 => 1}
-  resource = {'type' => 'vcpu', 'vnodename' => node_name}
-  cl.event_trace_add(resource, 'power', trace)
-  cl.event_manager_start
-  sleep(30)
-  cl.event_manager_stop
-end
+# # Now with a trace
+# Distem.client do |cl|
+#   trace = {10 => 1000, 15 => 2500, 20 => 1}
+#   resource = {'type' => 'vcpu', 'vnodename' => node_name}
+#   cl.event_trace_add(resource, 'power', trace)
+#   cl.event_manager_start
+#   sleep(30)
+#   cl.event_manager_stop
+# end
 
