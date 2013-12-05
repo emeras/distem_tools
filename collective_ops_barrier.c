a5fe5410d4410986466a793fccbfacc4c85b5441
emeras <joseph.emeras@imag.fr>
2013-12-05 13:34:38 +0100

    add a barrier version and original version of collective mpi code

---
+++ collective_ops_barrier.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <mpi.h>
+
+int main(int argc, char ** argv)
+{
+  int i, rank, out, nb_procs = 0;
+  int iter = 100;
+  int* array = 0;
+  double t_loop_start, t_loop_stop;
+  
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (rank == 0)
+  {
+    MPI_Comm_size(MPI_COMM_WORLD, &nb_procs);
+    printf("Running on %d procs\n", nb_procs);
+    array = (int*)calloc(nb_procs, sizeof(int));
+    for (i = 0; i < nb_procs; i++)
+      array[i] = nb_procs - i - 1;
+  }
+  t_loop_start = MPI_Wtime();
+  for (i = 0; i < iter; i++)
+  {
+    MPI_Bcast(&nb_procs, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(array, 1, MPI_INT, &out, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+      memset(array, 0, nb_procs);
+    MPI_Gather(&out, 1, MPI_INT, array, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  t_loop_stop = MPI_Wtime();
+  MPI_Finalize();
+  if (rank == 0) {
+    printf("Time in the loop: %1.2f\n", t_loop_stop-t_loop_start); 
+    fflush(stdout);
+  }  
+  
+  
+  return(0);
+}
+
