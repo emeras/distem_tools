400756537b5b0d891886136eaa530e58872831d0
emeras <joseph.emeras@imag.fr>
2013-12-06 15:25:53 +0100

    fix and clean

--- scalability.sh
+++ scalability.sh
@@ -63,7 +63,7 @@ NBVMTOT=$(($NBNODES * $VM))
 
 ssh root@$SERVER "cp $MPI_COLLECTIVE_FILE_PATH/collective_ops.c /root/"
 ssh root@$SERVER "cd /root/ ; mpicc -O3 collective_ops.c -o collective_ops"
-ssh root@$SERVER "NODES=$NODES while read i; do scp -p collective_ops $i:/tmp/distem/rootfs-shared/*/root; done < $NODES"
+ssh root@$SERVER "while read i; do scp -p collective_ops \$i:/tmp/distem/rootfs-shared/*/root; done < $NODES"
 # Then run mpi
 ssh root@$SERVER "rm run_times.log || true"
 ssh root@$SERVER "for i in {1..10}; do /usr/bin/time -f %e --output=run_times.log --append mpirun -machinefile $IPFILE --mca btl tcp,self ./collective_ops; done"
