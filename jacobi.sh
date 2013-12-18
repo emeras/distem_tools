77ed57ba9ed2082276e37915b16cb5eefb1703c1
emeras <joseph.emeras@imag.fr>
2013-12-18 13:33:05 +0100

    set longer sec time

--- jacobi.sh
+++ jacobi.sh
@@ -5,7 +5,7 @@ CHARM_HOME='/root/charm-6.5.1'
 TOTAL_CORES=1024
 JACOBI_PARAMS="2048 32"
 CHURN_DURATION=2000
-SEC_TIME=550
+SEC_TIME=600
 
 nohup ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64-syncft/tests/charm++/jacobi3d ; time ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/nodelist ./jacobi3d $JACOBI_PARAMS" > ./jacobi.log &
 
