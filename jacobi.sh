85cc47b02c11e81ef8bfcda9af0cb82fd2759226
emeras <joseph.emeras@imag.fr>
2013-12-20 10:18:46 +0100

    small changes for jacobi expe

--- jacobi.sh
+++ jacobi.sh
@@ -8,22 +8,21 @@ CHARM_HOME='/root/charm-6.5.1'
 #TOTAL_CORES=$1
 #TOTAL_CORES=1024
 TOTAL_CORES=256
-#JACOBI_PARAMS="2048 32"
-JACOBI_PARAMS="2048 2048 4096 512 512 512"
-CHURN_DURATION=300
+JACOBI_PARAMS="2048 2048 1024 256 256 256"
+#JACOBI_PARAMS="8192 8192 1 512 512 1"
+CHURN_DURATION=1200
 SEC_TIME=300
 
 taktuk -s -f /tmp/distem_vnodes_ip_hostnames broadcast exec [ hostname ]
 
-#nohup 
-ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64-syncft/tests/charm++/jacobi3d ; time ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/nodelist ./jacobi3d $JACOBI_PARAMS" #> ./jacobi.log &
+LOGFILE="jacobi_`date +%s`.log"
+
+nohup ssh root@`cat /tmp/distem_vnodes_ip | head -1` "cd $CHARM_HOME/net-linux-x86_64-syncft/tests/charm++/jacobi3d ; time ./charmrun ++p $TOTAL_CORES ++nodelist $CHARM_HOME/nodelist ./jacobi3d $JACOBI_PARAMS" > ./$LOGFILE &
 
 # security time to wait the experiment start
-#sleep $SEC_TIME
+sleep $SEC_TIME
 # failures
-#~jemeras/public/distem/distem_tools/churn_node.rb all $CHURN_DURATION &
+~jemeras/public/distem/distem_tools/churn_node.rb all $CHURN_DURATION &
 
 # grab results
-# DIR=expe_`date +%s`
-# mkdir $DIR
-# for i in `cat /tmp/distem_vnodes_ip` ; do scp -rp root@$i:$CHARM_HOME/net-linux-x86_64-syncft/tests/charm++/jacobi3d/jacobi3d.prj.* ./$DIR; done
+cp $LOGFILE ~jemeras/public/
